330b88889dfe9cba4c3a363d7d7461c51e378645
Cecilia <ccchimento@gmail.com>
2024-03-01 15:31:45 -0700

    completed lab

--- server/app.py
+++ server/app.py
@@ -19,25 +19,46 @@ class Signup(Resource):
     
     def post(self):
         json = request.get_json()
-        user = User(
-            username=json['username'],
-            password_hash=json['password']
-        )
+        user = User(username=json['username'])
+        user.password_hash = json['password'] 
         db.session.add(user)
         db.session.commit()
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+        user = User.query.filter(User.id == session['user_id']).first()
+        if not user:
+            return {}, 204
+        else:
+            return user.to_dict(), 200
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+        username = request.get_json()['username']
+
+        user = User.query.filter(User.username == username).first()
+        password = request.get_json()['password']
+
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+        return {'error' : 'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+    
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -24,5 +24,7 @@ class User(db.Model, SerializerMixin):
         return bcrypt.check_password_hash(
             self._password_hash, password.encode('utf-8'))
 
+    serialize_only = ('id', 'username')
+    
     def __repr__(self):
         return f'User {self.username}, ID: {self.id}'
